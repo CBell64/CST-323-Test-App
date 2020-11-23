72aa576b1d3f4f6c26cf0a8e842e9f3a4a499e84
Daniel Cender <danielcender@gmail.com>
2020-11-23 00:34:31 -0600

    finished activity 4 I guess

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'draft-js/dist/Draft.css';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ client/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Home = () => {
+  return <h2>Main Feed</h2>;
+};
+
+export default Home;
